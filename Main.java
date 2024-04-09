e70cf67cf3d0215070f6675bbcb97cd6af849ac0
MelissaH <iheartumedicdroid@gmail.com>
2024-04-09 15:57:51 -0700

    feat: add tic-tac-toe functionality

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 /**
  * Main
@@ -14,12 +16,27 @@ public class Main {
         // jFrame.setLocation(200, 200);
         // jFrame.setVisible(true);
 
-        MyJFrame jFrame1 = new MyJFrame("Testing the GUI");
-       
+        //MyJFrame jFrame1 = new MyJFrame("Testing the GUI");
+
         // JPanel jPanel = new JPanel();
         // jPanel.add(new JButton());
         // jFrame1.setContentPane(jPanel);
 
         // MyJFrame jFrame2 = new MyJFrame();
+        setLookAndFeel();
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
     }
+    
+    static void setLookAndFeel() {
+        try {
+        for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+        UIManager.setLookAndFeel(info.getClassName());
+        break;
+        }
+        }
+        } catch (Exception e) {
+        // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+        }
 }
