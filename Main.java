6d5baca447bb342a673892fedc55185884f9a496
MelissaH <iheartumedicdroid@gmail.com>
2024-04-09 14:20:32 -0700

    feat: add initial JFrame and JPanel

---
+++ Main.java
@@ -0,0 +1,25 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //JOptionPane.showMessageDialog(null,"Hello World", "Hello World Application", 2);
+        // JFrame jFrame = new JFrame();
+        // jFrame.setSize(1000, 1000);
+        // jFrame.setLocation(200, 200);
+        // jFrame.setVisible(true);
+
+        MyJFrame jFrame1 = new MyJFrame("Testing the GUI");
+       
+        // JPanel jPanel = new JPanel();
+        // jPanel.add(new JButton());
+        // jFrame1.setContentPane(jPanel);
+
+        // MyJFrame jFrame2 = new MyJFrame();
+    }
+}
